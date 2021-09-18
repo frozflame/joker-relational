529f02df752a74b6a2631de9bd025b0177a59793
frozflame <frozflame@outlook.com>
2021-09-18 19:07:07 +0800

    SQLInterface.wait_until_server_ready(), .exists, .create_database

--- joker/relational/interfaces.py
+++ joker/relational/interfaces.py
@@ -2,14 +2,17 @@
 # coding: utf-8
 
 import logging
+import time
 
 import sqlalchemy
+import sqlalchemy.exc
 from sqlalchemy import MetaData
 from sqlalchemy.engine import Engine
 
 _logger = logging.getLogger(__name__)
 
 
+# noinspection SqlNoDataSourceInspection
 class SQLInterface:
     def __init__(self, engine: Engine, metadata: MetaData):
         self.engine = engine
@@ -23,6 +26,7 @@ class SQLInterface:
     def execute_script(self, path: str):
         with self.engine.connect() as conn:
             conn.execute('COMMIT;')
+            _logger.debug('execute sql script: %s', path)
             return conn.execute(open(path).read())
 
     def execute(self, statement):
@@ -60,3 +64,36 @@ class SQLInterface:
                 self.execute(f'REFRESH MATERIALIZED VIEW {v};')
             _logger.info('mview refreshed: %s', v)
         return mviews
+
+    def wait_until_server_ready(self, timeout: int = 30, period: int = 3):
+        start = time.time()
+        for _ in range(0, timeout, period):
+            try:
+                # language=SQL
+                self.execute('SELECT 1;').scalar()
+                _logger.info('database server is ready now!')
+                return
+            except sqlalchemy.exc.OperationalError as exc:
+                _logger.info(
+                    'failed to connect to %s -- %s',
+                    self.engine.url, exc.args[0],
+                )
+                time.sleep(period)
+        if (remaining := timeout + start - time.time()) > 0:
+            time.sleep(remaining)
+
+    def exists(self, database: str):
+        sql = f"SELECT 1 FROM pg_database WHERE datname='{database}';"
+        try:
+            return bool(self.execute(sql).scalar())
+        except sqlalchemy.exc.OperationalError:
+            return False
+
+    def create_database(self, name: str):
+        if self.exists(name):
+            _logger.info('database exists already, creation skipped')
+            return
+        with self.engine.connect():
+            # language=SQL
+            sql = f'CREATE DATABASE {name};'
+            self.execute(sql)
